c8e10dd882e15c3760e2cade62dfd739fa4a5f67
krishmoran <43583683+krishmoran@users.noreply.github.com>
2020-01-13 15:32:40 -0500

    commenting and syntax

--- HBVTicketing.py
+++ HBVTicketing.py
@@ -3,25 +3,29 @@ import pandas as pd
 import win32com.client as win32
 
 outlook = win32.Dispatch('outlook.application')
+
+# reads user excel spreadsheet, must have column 'User" which is full name of person,
+# and 'Access' column, which is a column describing access level, e.g. 'HVW Marketing BASIC' 
 df = pd.read_csv("UAL_HV.csv")
 
-users=df.User
+# establishes dataframes of the pertinent columns
+users = df.User
 departments = df.Access
     
-    
+# defines the subject of the ticket 
 def makeSubject(name):
     print("Harbourview PROD Access to: " + name + "\n")
     
-    
+# defines the message body of the ticket     
 def makeBody(name, access):
     print("Employee Name: " + name +
           "\n HarbourView Access\n" + 
           "Environment: PROD\n" +
           "Project(s): HarbourView Next Gen PROD\n" +
           "Privelages: NA\n"
-          + "Permissions: " + access 
-+ "\n----------------------------------------------------------")
+          + "Permissions: " + access)
 
+# sends the ticket via email to IT utilizing the outlook desktop client
 def sendTickets():
     for i in range (len(users)):
         mail = outlook.CreateItem(0)
