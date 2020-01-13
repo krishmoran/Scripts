bf1d19b236307ed1e4f1e8a21c44631b5eb1a955
krishmoran <43583683+krishmoran@users.noreply.github.com>
2020-01-13 16:40:12 -0500

    finished, awaiting proper email

--- HBVTicketing.py
+++ HBVTicketing.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import win32com.client as win32
 
@@ -6,7 +5,12 @@ outlook = win32.Dispatch('outlook.application')
 
 # reads user excel spreadsheet, must have column 'User" which is full name of person,
 # and 'Access' column, which is a column describing access level, e.g. 'HVW Marketing BASIC' 
-df = pd.read_csv("UAL_HV.csv")
+
+print("Input CSV must have a 'User' column which is the full name of a person needing access, e.g. John Doe \n" +
+          "and an 'Access' column, which is a column describing access levels, like 'HVW Marketing BASIC'")
+
+df = pd.read_csv(input("Enter the exact filepath of the speadsheet with users and their access levels: \n"))
+# df = pd.read_csv('C:/Users/kmoran/Documents/UAL_HV.csv')
 
 # establishes dataframes of the pertinent columns
 users = df.User
@@ -14,34 +18,33 @@ departments = df.Access
     
 # defines the subject of the ticket 
 def makeSubject(name):
-    print("Harbourview PROD Access to: " + name + "\n")
+    return("HarbourView PROD Access Request For: " + name + "\n")
     
 # defines the message body of the ticket     
 def makeBody(name, access):
-    print("Employee Name: " + name +
-          "\n HarbourView Access\n" + 
-          "Environment: PROD\n" +
-          "Project(s): HarbourView Next Gen PROD\n" +
-          "Privelages: NA\n"
+    return("Employee Name: " + name +
+          "\n\n HarbourView Access\n\n" + 
+          "Environment: PROD\n\n" +
+          "Project(s): HarbourView Next Gen PROD\n\n" +
+          "Privelages: NA\n\n"
           + "Permissions: " + access)
 
 # sends the ticket via email to IT utilizing the outlook desktop client
 def sendTickets():
-    for i in range (len(users)):
+    for i in range(2):
+        subject = makeSubject(users[i])
+        body = makeBody(users[i], departments[i])
+    
         mail = outlook.CreateItem(0)
-        mail.To = 'HARBOURVESTITEMAIL@harbourvest.com'
-        mail.Subject = makeSubject(users[i])
-        mail.Body = makeBody(users[i], departments[i])
+        mail.To = 'kmoran@harbourvest.com'        
+        mail.Subject = subject
+        mail.Body = body
         mail.Send()
+        print("Access Request Sent for: " + users[i])
+    
         
 def main():
     sendTickets()
     
 if __name__ == '__main__': 
     main()
-
-
-
-
-
-
